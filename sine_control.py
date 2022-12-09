dbd9d958dd962a14ab6d486fab40b59d72206a48
Lucas Lemos <llcf@ic.ufal.br>
2022-12-08 22:03:46 -0300

    create lands functions

--- sine_control.py
+++ sine_control.py
@@ -1,8 +1,24 @@
 from math import *
 from server_base import Server
+import numpy as np
 
-dt = 0.06;
+from lands_functions import *
 
+uStar = 0
+uMax = 1
+dt = 0.06
+    
+def computeBestU():
+	uStar = 0
+
+	dMin = 100000
+
+	# for ut1 in np.arange(-uMax, uMax, uMax / 10):
+	# 	for ut2 in np.arange(-uMax, uMax, uMax / 10):
+	# 		position = 
+
+
+print(landsFunctions(1, 5, 6, -0.))
 class Control:
 
 	def __init__(self, verbose = False):
